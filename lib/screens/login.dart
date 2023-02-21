e4fa8f23aa4b514370c7e0a33164f351d39f7fcd
ledjoric <ledjoricvermont@gmail.com>
2023-02-21 16:47:33 +0800

    Login Page

---
+++ lib/screens/login.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({super.key});
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LOGIN'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Username'),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text('Password'),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            GestureDetector(
+              child: const Text(
+                'Forgot Password',
+                style: TextStyle(
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+              onTap: () {
+                //Navigator.push(context, MaterialPageRoute(builder: (context) => const ))
+              },
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            FilledButton(
+              onPressed: () {},
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
